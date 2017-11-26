eac784d4f209126f580f4db8603d525bd7719c28
xiawenjie <xia.wenjie@qq.com>
2017-11-26 08:53:52 +0800

    initial commit

---
+++ src/com/xp/tij/demo/HelloWorld.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.xp.tij.demo;
+
+/**
+ * @author I074186
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
