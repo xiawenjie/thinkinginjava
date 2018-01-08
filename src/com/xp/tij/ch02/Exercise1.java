463ba9ed53932c71768b6dc6d7990bee64fd0426
xiawenjie <xia.wenjie@qq.com>
2018-01-08 22:57:16 +0800

    ch2 exercise1

---
+++ src/com/xp/tij/ch02/Exercise1.java
@@ -0,0 +1,11 @@
+package com.xp.tij.ch02;
+
+public class Exercise1 {
+	static int i;
+	static char c;
+	
+	public static void main() {
+		System.out.println("i="+i);
+		System.out.println("c="+c);
+	}
+}
