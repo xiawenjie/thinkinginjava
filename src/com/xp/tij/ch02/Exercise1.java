a0938308418cbfe8f3beae9b8712f383c432461e
xiawenjie <xia.wenjie@qq.com>
2018-01-08 23:19:00 +0800

    ch02

--- src/com/xp/tij/ch02/Exercise1.java
+++ src/com/xp/tij/ch02/Exercise1.java
@@ -1,3 +1,5 @@
+//TIJ Chapter2 Exercise1
+//Create a class to verify JAVA's default initialization
 package com.xp.tij.ch02;
 
 public class Exercise1 {
---
+++ src/com/xp/tij/ch02/Exercise2.java
@@ -0,0 +1,9 @@
+//TIJ Chapter2 Exercise2
+//Create a hello world program
+package com.xp.tij.ch02;
+
+public class Exercise2 {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
