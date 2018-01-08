2b040943297f376862e4a4bd00fee2b4581e8938
xiawenjie <xia.wenjie@qq.com>
2018-01-08 23:00:06 +0800

    ch02

--- src/com/xp/tij/ch02/Exercise1.java
+++ src/com/xp/tij/ch02/Exercise1.java
@@ -4,7 +4,7 @@ public class Exercise1 {
 	static int i;
 	static char c;
 	
-	public static void main() {
+	public static void main(String[] args) {
 		System.out.println("i="+i);
 		System.out.println("c="+c);
 	}
