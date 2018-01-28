e4529a5de74f70f3e71fe48014e602bf772eef86
xiawenjie <xia.wenjie@qq.com>
2018-01-28 21:16:46 +0800

    project

---
+++ src/com/xp/tij/ch02/Exercise3.java
@@ -0,0 +1,20 @@
+package com.xp.tij.ch02;
+
+public class Exercise3 {
+
+	public static void main(String[] args) {
+		class ATypeName{
+
+			int i=5;
+			char c='a';
+			
+			
+		}
+		
+		ATypeName aTypeName = new ATypeName();
+		System.out.println("aTypeName.c="+aTypeName.c);;
+		System.out.println("aTypeName.i="+aTypeName.i);;
+
+	}
+
+}
